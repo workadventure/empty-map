b8eb6e94fc659c0f264288ca61cea93d89147453
ValdoTR <vteixeiraromao@gmail.com>
2025-01-15 11:06:13 +0000

    Deploying to gh-pages from master @ a5d7093196eb305314d21ae231983652e3d663f3 🚀

---
+++ assets/main-c977fde9.js
@@ -0,0 +1,2 @@
+console.log("Script started successfully");WA.onInit().then(()=>{console.log("Scripting API ready"),console.log("Player tags: ",WA.player.tags)}).catch(o=>console.error(o));
+//# sourceMappingURL=main-c977fde9.js.map
